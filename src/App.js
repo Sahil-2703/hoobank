65c9f2a9e472247ef55c89962da8cfa85636ff3f
Sahil-2703 <117385977+Sahil-2703@users.noreply.github.com>
2023-02-15 19:49:50 +0530

    hoobank 15-02-2023

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styles from './style'
+import Navbar from './components/Navbar'
+import Billing from './components/Billing'
+import Business from './components/Business'
+import Cardeall from './components/CarDeall'
+import Clients from './components/Clients'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Stats from './components/Stats'
+import Testimonials from './components/Testimonials'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <Cardeall/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
